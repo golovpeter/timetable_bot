6b56e1c4628c15e085ab4ff27672d901b523946a
golovpeter <petya-golov@mail.ru>
2020-08-07 13:40:57 +0300

    Refactoring code and create new files for all classes

--- handlers/text_handler.py
+++ handlers/text_handler.py
@@ -1,8 +1,9 @@
 import os
 
 from config import bot, user_cache
-from constants import class_letters, class_numbers, days
+from constants import class_letters, class_numbers, days, lower_classes_days
 from utils import create_buttons
+import json
 
 
 @bot.message_handler(content_types=['text'])
@@ -20,8 +21,10 @@ def get_message(message):
 def handle_class_number(message):
     class_index = message.text.split()[0]
     user_cache[message.from_user.id] = class_index
-
-    buttons = create_buttons(class_letters, 2)
+    if int(class_index) >= 8:
+        buttons = create_buttons(class_letters, 2)
+    else:
+        buttons = create_buttons(lower_classes_days, 2)
 
     bot.send_message(message.chat.id, 'Выберите букву', reply_markup=buttons)
 
@@ -39,12 +42,18 @@ def handle_class_letter(message):
 
 
 def handel_day_of_the_week(message):
-    day_of_the_week = message.text
     file_path = user_cache[message.from_user.id]
 
     if os.path.isfile(file_path):
-        # TODO
-        # Вывести расписание, отобразить снова дни недели и кнопку возврата
-        pass
+        with open(file_path, encoding='utf-8') as json_file:
+            timetable = json.load(json_file)
+            i = days.index(message.text)
+            lessons = timetable[i]['lessons']
+            result = ''
+            for lesson in lessons:
+                result += '{}.{} | {}:{} \n'.format(str(lesson['lessonNumber']), lesson['lessonName'],
+                                                    'Кабинет ', lesson['roomNumber'])
+            bot.send_message(message.chat.id, result)
+
     else:
         bot.send_message(message.chat.id, "Расписание для выбранного класса не найдено")
