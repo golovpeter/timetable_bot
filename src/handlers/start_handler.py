e389e42fd52fa807c571b0ee51f1c830c4dabf7e
golovpeter <petya-golov@mail.ru>
2020-08-08 13:51:29 +0300

    Added test logging

--- src/handlers/start_handler.py
+++ src/handlers/start_handler.py
@@ -1,10 +1,15 @@
+import telebot
+
 from config import bot
-from utils import create_buttons
 from constants import CLASS_NAMES
+from utils import create_buttons
+
+logger = telebot.logger
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    logger.info("Start message received from user {}".format(message.from_user.username))
     buttons = create_buttons(CLASS_NAMES, 3, has_return=False)
 
     bot.send_message(message.chat.id,
